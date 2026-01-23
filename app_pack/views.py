e8ec421113275cb40dea77a2101d4999fb779823
alina1412 <alina1412@mail.ru>
2026-01-23 16:48:04 +0300

    style

--- app_pack/views.py
+++ app_pack/views.py
@@ -1,13 +1,9 @@
 import re
+import logging
 
 from flask import (
-    Flask,
-    flash,
-    redirect,
     render_template,
     request,
-    session,
-    url_for,
 )
 from sqlalchemy import text
 
@@ -19,26 +15,27 @@ def db_get(sql):
     return res.fetchall()
 
 
-@app.route("/", methods=["GET"])
-def forums():
-    """главная с форумами по категориям"""
-    all_categories = db_get("""SELECT cat_id, cat_title 
-    FROM dennikov.phpbb_1categories ORDER BY cat_order;""")
+@app.route("/")
+def forums_view():
+    """Главная с форумами по категориям"""
+    all_categories = db_get("""
+                SELECT cat_id, cat_title 
+                FROM dennikov.phpbb_1categories 
+                ORDER BY cat_order;
+                            """)
 
-    d2 = [(cat.cat_id, cat.cat_title) for cat in all_categories]
-    all_cat_id = [int(x[0]) for x in d2]
+    all_cat_id = [int(cat.cat_id) for cat in all_categories]
 
     categs_have_forums = {}
 
-    sql = """select phpbb_1forums.forum_id, forum_name, forum_desc, 
+    sql = """SELECT phpbb_1forums.forum_id, forum_name, forum_desc, 
         forum_posts as forum_posts_count, forum_topics as forum_topics_count
-        from dennikov.phpbb_1forums
+        FROM dennikov.phpbb_1forums
         where cat_id = {};"""
 
     for cat_id in all_cat_id:
         res = db_get(sql.format(cat_id))
         """forum_ids_in_cat = [x["forum_id"] for x in res]"""
-
         categs_have_forums[cat_id] = res
 
     context = {
@@ -50,20 +47,22 @@ def forums():
 
 
 @app.route("/topics/<forum_id>/<topic_id>")
-def posts(forum_id, topic_id):
+def posts_view(forum_id, topic_id):
     """Показывает список постов в топике в форуме"""
     try:
         forum_id = int(forum_id)
         topic_id = int(topic_id)
     except ValueError as err:
-        print(str(err))
+        logging.error(str(err))
         return
-    print(forum_id, topic_id, "------")
-    sql = f"""select phpbb_1posts.post_id, phpbb_1posts.topic_id, 
+
+    logging.debug(f"{forum_id}, {topic_id}")
+
+    sql = f"""SELECT phpbb_1posts.post_id, phpbb_1posts.topic_id, 
     phpbb_1users.username, phpbb_1posts.poster_id,
     to_char(date(to_timestamp(post_time)),'DD-MM-YYYY') AS "time2",
                     bbcode_uid, post_subject, post_text, topic_title
-        from dennikov.phpbb_1posts 
+        FROM dennikov.phpbb_1posts 
         left join dennikov.phpbb_1posts_text on phpbb_1posts_text.post_id = phpbb_1posts.post_id
         left join dennikov.phpbb_1topics on phpbb_1posts.topic_id = phpbb_1topics.topic_id
         left join dennikov.phpbb_1users on user_id = phpbb_1posts.poster_id
@@ -75,8 +74,8 @@ def posts(forum_id, topic_id):
     votes = {}
     if topic_id in [87, 96, 100, 146, 172, 192]:
         sql = f"""    
-        select  vote_option_text, vote_result
-        from dennikov.phpbb_1vote_desc
+        SELECT  vote_option_text, vote_result
+        FROM dennikov.phpbb_1vote_desc
         left join dennikov.phpbb_1vote_results 
            on phpbb_1vote_desc.vote_id = phpbb_1vote_results.vote_id
         where topic_id = {topic_id}
@@ -87,17 +86,22 @@ def posts(forum_id, topic_id):
         post_text = post.get("post_text", "")
         if not post_text:
             post_text = ""
-        print("post_text---------------", post_text)
+        logging.debug(f"post_text {post_text}")
+
         post_text = post_text.replace("\r\n", "<br>")
         post_text = post_text.replace("\n", "<br>")
+
         for elem in app.config["smilies"]:
             img = f"""<img src="/static/img/{elem.smile_url}" type="image/ico">"""
             post_text = post_text.replace(elem.code, img)
 
         pattern = r"""^(.*)?(\[quote:[a-z0-9\-]{10})([=]["].*["]\])(.*)?(\[\/quote:[a-z0-9\-]{10}\])(.*)?"""
         groups_ = re.match(pattern, post_text)
+
         if groups_:
-            print([(i, g) for i, g in enumerate(groups_.groups())])
+            logging.debug(
+                f"groups: {[(i, g) for i, g in enumerate(groups_.groups())]}"
+            )
             d = {}
             for i, g in enumerate(groups_.groups()):
                 d[i] = g
@@ -114,7 +118,7 @@ def posts(forum_id, topic_id):
             )
             post_text = post_text.replace(d[2], "")
             post_text = post_text.replace(d[4], """\n</div>\n""")
-            print(post_text)
+            logging.debug(f"processed post_text: {post_text}")
 
         def for_b_tag(post_text):
             pattern = r"""\[b:[a-z0-9\-]{10}\]"""
@@ -175,8 +179,9 @@ def posts(forum_id, topic_id):
         )
         for post in posts
     ]
+
     for i, elem in enumerate(posts):
-        print(elem, "----------")
+        logging.debug(f"elem: {elem}")
         post_text = change_quotes(elem)
         posts[i]["post_text"] = post_text
 
@@ -192,17 +197,17 @@ def posts(forum_id, topic_id):
 
 
 @app.route("/topics/<forum_id>")
-def topics(forum_id):
-    """показывает список топиков в форуме"""
-    # request.args.get('listOfObjects')
+def topics_view(forum_id):
+    """Показывает список топиков в форуме"""
     try:
         forum_id = int(forum_id)
     except ValueError as err:
-        print(str(err))
+        logging.error(str(err))
         return
-    print(forum_id, "forum_id------")
-    sql = f"""select forum_name, topic_id, topic_title, topic_replies
-        from dennikov.phpbb_1topics
+    logging.debug(f"{forum_id} forum_id")
+
+    sql = f"""SELECT forum_name, topic_id, topic_title, topic_replies
+        FROM dennikov.phpbb_1topics
         left join dennikov.phpbb_1forums 
                   on phpbb_1forums.forum_id = phpbb_1topics.forum_id
         where phpbb_1topics.forum_id = {forum_id}
@@ -215,7 +220,6 @@ def topics(forum_id):
         "title": topics[0].forum_name if topics else "",
         "forum_id": forum_id,
     }
-
     return render_template("topics.html", **context)
 
 
@@ -224,34 +228,37 @@ def users(user_id):
     """Показывает user"""
     try:
         user_id = int(user_id)
-        print(user_id, "---")
+        logging.debug(f"{user_id}")
         if user_id > 10**8:
             raise ValueError
     except ValueError as err:
-        print(str(err))
+        logging.error(str(err))
         return
+
     forum_id = request.args.get("forumId")
     topic_id = request.args.get("topicId")
     return_to = {"forum_id": forum_id, "topic_id": topic_id}
 
-    print(return_to, "return_to-----")
+    logging.debug(f"return_to: {return_to}")
+
     sql = f"""
-        select username, 
+        SELECT username, 
         to_char(date(to_timestamp(user_regdate)),'DD-MM-YYYY') as registered, 
         user_posts as user_posts_count, user_from, user_occ, user_interests
-        from dennikov.phpbb_1users   
+        FROM dennikov.phpbb_1users   
         where user_id = {user_id};    
     """
     user = db_get(sql)
     try:
         user = user[0]
     except Exception as err:
-        print(str(err))
+        logging.error(str(err))
         return
-    print(user)
+
+    logging.debug(f"user: {user}")
+
     context = {
         "user": user,
         "return_to": return_to,
     }
-
     return render_template("user.html", **context)
